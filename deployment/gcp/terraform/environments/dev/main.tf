fd6255146e087708ef19d7817bc99ea518db272b
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-05-18 21:14:17 -0400

    Create GKE Autopilot cluster

--- deployment/gcp/terraform/environments/dev/main.tf
+++ deployment/gcp/terraform/environments/dev/main.tf
@@ -5,9 +5,14 @@ module "artifactregistry" {
   region     = var.region
 }
 
-resource "null_resource" "docker_publish" {
-  provisioner "local-exec" {
-    command     = "./docker-publish.sh ${local.image_repository}"
-    working_dir = "../../../scripts"
-  }
+module "gke" {
+  source = "../../modules/gke"
+
+  project_id = var.project_id
+  region     = var.region
+  network    = var.network
+  gke_ip_cidr_range = var.gke_ip_cidr_range
+  gke_ip_range_pods = var.gke_ip_range_pods
+  gke_ip_range_services = var.gke_ip_range_services
+  master_authorized_networks = var.master_authorized_networks
 }
---
+++ deployment/gcp/terraform/modules/gke/main.tf
@@ -0,0 +1,52 @@
+data "google_client_config" "default" {}
+
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "kubernetes" {
+  host                   = "https://${module.users_service_gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.users_service_gke.ca_certificate)
+}
+
+resource "google_project_service" "compute_api" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "container_api" {
+  service            = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_compute_subnetwork" "users_service_gke" {
+  name          = "users-service-gke"
+  ip_cidr_range = var.gke_ip_cidr_range
+  network       = var.network
+  secondary_ip_range {
+    range_name    = local.ip_range_pods
+    ip_cidr_range = var.gke_ip_range_pods
+  }
+  secondary_ip_range {
+    range_name    = local.ip_range_services
+    ip_cidr_range = var.gke_ip_range_services
+  }
+}
+
+module "users_service_gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-private-cluster"
+  project_id                 = var.project_id
+  region = var.region
+  name                       = "users-service-gke-1"
+  network                    = var.network
+  subnetwork                 = google_compute_subnetwork.users_service_gke.name
+  ip_range_pods              = local.ip_range_pods
+  ip_range_services          = local.ip_range_services
+  enable_private_endpoint    = false
+  enable_private_nodes       = true
+  enable_vertical_pod_autoscaling = true
+  master_authorized_networks = var.master_authorized_networks
+}
